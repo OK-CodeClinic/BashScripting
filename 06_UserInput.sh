a6c2483265418dcb4be4124fff1954bae057e45e
Kehinde Omokungbe <kehindefranklin@gmail.com>
2023-03-27 12:08:20 +0000

    Taking user input

---
+++ 06_UserInput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter your skill"
+read SKILL
+
+echo "Your $SKILL Skill is of a very high demand in the tech market."
+
+read -p 'USERNAME: ' USR
+read -sp 'PASSWORD: ' PASS
+
+echo
+
+echo "Login Succefully. Welcome $USR,"
